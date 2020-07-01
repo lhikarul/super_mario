e9b4570f53c0e7076cafbee0a05c235a9ebb52d0
lhiakrul <711hikaru@gmail.com>
2020-07-01 22:07:13 +0800

    add: init

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    mode: 'development',
+    devServer: {
+        contentBase: './docs',
+        open: true
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname,'docs')
+    },
+    plugins:[new HtmlWebpackPlugin({
+        template: 'src/index.html'
+    })],
+    resolve: {
+        alias: {
+            image: path.resolve(__dirname,'src/image')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|png)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: '/images'
+                    }
+                }
+            }
+        ]
+    }
+}
